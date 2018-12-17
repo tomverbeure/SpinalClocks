e37aa3537ed8ced466aa01bde38984e5ef875416
Tom Verbeure <hombre+github@gmail.com>
2018-12-17 03:45:42 +0000

    s/var/val/

--- src/main/scala/clocks/Top.scala
+++ src/main/scala/clocks/Top.scala
@@ -6,10 +6,10 @@ import spinal.lib._
 
 class Top() extends Component {
 
-    var io = new Bundle {
-        var led_red         = out(Bool)
-        var led_green       = out(Bool)
-        var switch          = in(Bool)
+    val io = new Bundle {
+        val led_red         = out(Bool)
+        val led_green       = out(Bool)
+        val switch          = in(Bool)
     }
 
     io.led_red   := RegNext(io.switch) init(False)
