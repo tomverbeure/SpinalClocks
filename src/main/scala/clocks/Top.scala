ba6f044e2cba38051cc0d14eb62216618c3aaf11
Tom Verbeure <hombre+github@gmail.com>
2018-12-17 03:48:09 +0000

    s/var/val/

--- src/main/scala/clocks/Top.scala
+++ src/main/scala/clocks/Top.scala
@@ -6,31 +6,31 @@ import spinal.lib._
 
 class Top() extends Component {
 
-    var io = new Bundle {
-        var led_red         = out(Bool)
-        var led_green       = out(Bool)
-        var led_blue        = out(Bool)
-        var switch          = in(Bool)
+    val io = new Bundle {
+        val led_red         = out(Bool)
+        val led_green       = out(Bool)
+        val led_blue        = out(Bool)
+        val switch          = in(Bool)
     }
 
     // Create a VO clock domain with clock and reset coming straight from the toplevel
-    var voClkDomain = ClockDomain.external("vo")
+    val voClkDomain = ClockDomain.external("vo")
     voClkDomain.reset.setName("vo_reset_")
 
     // Use the VO clock domain to create 2 new signals: vo_div2_clk and vo_div2_reset_
     // These are currently ordinary signals and not yet understood to be clock or reset.
-    var vo_domain = new ClockingArea(voClkDomain) {
-        var vo_div2_clk     = Bool
-        var vo_div2_reset_  = Bool
+    val vo_domain = new ClockingArea(voClkDomain) {
+        val vo_div2_clk     = Bool
+        val vo_div2_reset_  = Bool
 
-        var u_clk_div2 = new ClkDiv2
+        val u_clk_div2 = new ClkDiv2
         u_clk_div2.io.clk_div2    <> vo_div2_clk
         u_clk_div2.io.reset_div2_ <> vo_div2_reset_
     }
 
     // Now create a new clock domain "vo_div2". The clock and reset of this domain will be internal signals.
     // It's up to the programmer to later tell exactly which internal signals will be used for that.
-    var voDiv2ClkDomain = ClockDomain.internal("vo_div2")
+    val voDiv2ClkDomain = ClockDomain.internal("vo_div2")
 
     // Tell the clock domain which ordinary signals will be used as clock and reset. We are using the
     // earlier generated signals for this.
@@ -43,12 +43,12 @@ class Top() extends Component {
     voDiv2ClkDomain.reset.setName("vo_div2_reset_")
 
     // We can now use this new clock domain to drive regular logic.
-    var vo_div2_domain = new ClockingArea(voDiv2ClkDomain) {
+    val vo_div2_domain = new ClockingArea(voDiv2ClkDomain) {
 
         io.led_red   := RegNext(io.switch) init(False)
         io.led_green := RegNext(io.switch)
 
-        var u_sub1 = new Sub1
+        val u_sub1 = new Sub1
         u_sub1.io.switch        <> io.switch
         u_sub1.io.switch_reg    <> io.led_blue
     }
@@ -57,13 +57,13 @@ class Top() extends Component {
 
 class ClkDiv2 extends Component {
 
-    var io = new Bundle {
-        var clk_div2       = out(Bool)
-        var reset_div2_    = out(Bool)
+    val io = new Bundle {
+        val clk_div2       = out(Bool)
+        val reset_div2_    = out(Bool)
     }
 
-    var clk_div2        = Reg(Bool) init(False)
-    var reset_div2_     = Reg(Bool) init(False)
+    val clk_div2        = Reg(Bool) init(False)
+    val reset_div2_     = Reg(Bool) init(False)
 
     clk_div2    := ~clk_div2
     reset_div2_ := clk_div2 | reset_div2_
@@ -74,8 +74,8 @@ class ClkDiv2 extends Component {
 
 class Sub1() extends Component {
 
-    var io = new Bundle {
-        var switch          = in(Bool)
+    val io = new Bundle {
+        val switch          = in(Bool)
         val switch_reg      = out(Bool)
     }
 
